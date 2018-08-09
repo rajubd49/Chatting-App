0eb4ec0e91cd8a5e0735d5b1f6b13b1f375cb2ce
Harish-Uz-Jaman Mridha Raju <mridha@bs-23.net>
2018-08-09 14:35:05 +0600

    Send message and save to firebase database

--- Chatting App/Controller/MessageLogController.swift
+++ Chatting App/Controller/MessageLogController.swift
@@ -7,13 +7,19 @@
 //
 
 import UIKit
+import Firebase
 
-class MessageLogController: UIViewController {
+class MessageLogController: UIViewController,UITextFieldDelegate {
 
+    @IBOutlet weak var messageTextField: UITextField!
+    @IBOutlet weak var sendButton: UIButton!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+        messageTextField.delegate = self
+        let paddingView = UIView(frame: CGRect(x: 0, y: 0, width: 12, height: messageTextField.frame.height))
+        messageTextField.leftView = paddingView
+        messageTextField.leftViewMode = .always
     }
 
     override func didReceiveMemoryWarning() {
@@ -21,4 +27,23 @@ class MessageLogController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    @IBAction func sendAction(_ sender: Any) {
+        let databaseReference = Database.database().reference().child("messages")
+        let childReference = databaseReference.childByAutoId()
+        let values = ["text": messageTextField.text!]
+        childReference.updateChildValues(values)
+        messageTextField.text = nil
+    }
+    
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        sendAction(UIButton.self)
+        return true
+    }
+    
+    func textField(_ textField: UITextField, shouldChangeCharactersIn range: NSRange, replacementString string: String) -> Bool {
+        guard let text = textField.text else { return true }
+        let maxLength = text.utf16.count + string.utf16.count - range.length
+        sendButton.isEnabled = maxLength > 0
+        return true
+    }
 }
